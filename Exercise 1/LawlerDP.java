aa3410d0f1a28c9a4a453efd2bc6194f53666308
Vasco de Bruijn <V.deBruijn@student.tudelft.nl>
2018-09-18 12:07:53 +0200

    Added framework class

---
+++ Exercise 1/LawlerDP.java
@@ -0,0 +1,18 @@
+
+public class LawlerDP {
+	private int numJobs;
+	private int[][] jobs;
+	
+	public LawlerDP(ProblemInstance instance) {
+		numJobs = instance.getNumJobs();
+		jobs = instance.getJobs();		
+	}
+	
+	public Schedule getSchedule() {
+		Schedule bestSchedule = null;
+		
+		//magic happens here
+		
+		return bestSchedule;
+	}
+}
