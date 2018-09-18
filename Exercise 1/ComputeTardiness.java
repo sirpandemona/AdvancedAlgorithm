e4bc8339af1e661137128d91a09eb94e6ce06a52
Vasco de Bruijn <V.deBruijn@student.tudelft.nl>
2018-09-18 13:03:56 +0200

    Added test functions

--- Exercise 1/ComputeTardiness.java
+++ Exercise 1/ComputeTardiness.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ComputeTardiness {	
 	public static ProblemInstance readInstance(String filename){
@@ -35,7 +38,7 @@ public class ComputeTardiness {
 
 	// reads a problem, and outputs the result of both greedy and best-first
     public static void main (String args[]) {
-		ProblemInstance instance = readInstance(args[0]);
+		/*ProblemInstance instance = readInstance(args[0]);
 		
 		Greedy greedy = new Greedy(instance);
 		Schedule greedySchedule = greedy.getSchedule();
@@ -43,6 +46,34 @@ public class ComputeTardiness {
 		
 		BestFirst bestFirst = new BestFirst(instance);
 		Schedule bestFirstSchedule = bestFirst.getSchedule();
-		System.out.println(bestFirstSchedule.getTardiness());
+		System.out.println(bestFirstSchedule.getTardiness());*/
+    	
+    	runTests();
 	}
+    
+    public static void runTests() {
+    	Path currentRelativePath = Paths.get("");
+    	String testfolder = currentRelativePath.toAbsolutePath().toString()+"\\Exercise 1\\instances"; 
+    	testfolder = testfolder.replace("\\", "/");
+    	System.out.println(testfolder);
+    	File folder = new File(testfolder);
+    	
+    	File[] testfiles = folder.listFiles();
+    	for (int i = 0; i < testfiles.length; i++) {
+    		String fileName = testfiles[i].getName();
+    		runTest(testfolder+"\\"+fileName);
+
+		}
+    }
+    
+    public static void runTest(String filename) {
+    	ProblemInstance instance = readInstance(filename);
+    	Greedy greedy = new Greedy(instance);
+		Schedule greedySchedule = greedy.getSchedule();
+		System.out.println(greedySchedule.getTardiness());
+		
+		BestFirst bestFirst = new BestFirst(instance);
+		Schedule bestFirstSchedule = bestFirst.getSchedule();
+		System.out.println(bestFirstSchedule.getTardiness());
+    }
 }
