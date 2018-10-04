e0870975fe02aa9a21dd524668ce71eb132a2b99
ivovankreveld@hotmail.com <ivovankreveld@hotmail.com>
2018-10-04 10:33:55 +0200

    SequenceJobs finish and making it more readable

--- Exercise 1/LawlerDP.java
+++ Exercise 1/LawlerDP.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -116,33 +117,35 @@ public class LawlerDP {
 	
 	public int SequenceJobs() {		
 		sortJobsByDueTime();
-		List<int[]> L = generateStates(0, jobs.length, -1, 0);
+		ArrayList<int[]> L = generateStates(0, jobs.length, -1, 0);
+		Collections.reverse(L);
 		
 		HashMap<int[],Integer> T = new HashMap<int[],Integer>();
 		
-		for(int i = 0; i < L.size(); i++) {
-			int[] S = L.get(i);							// S = [i,j,k,t]
-			if(S[1]-S[0] == 0) {
+		for(int[] S : L) {							
+			int i = S[0];
+			int j = S[1];
+			int oldk = S[2];
+			int t = S[3];
+			int k = retrieveKPrime(retrieveSubset(i,j,oldk));
+			if(j-i == 0) {
 				T.put(S,0);
 			}
-			if(S[1]-S[0] == 1) {
-				T.put(S, Math.max(0,S[3]+jobs[i][0]-jobs[i][1]));
+			if(j-i == 1) {
+				T.put(S, Math.max(0,t+jobs[i][0]-jobs[i][1]));
 			}
 			T.put(S, Integer.MAX_VALUE);
-			for(int delta = 0; delta < S[1]-S[2]; delta++) {
-				int C = S[3];
-				for(int j = 0; j < S[2]+delta; j++) {
-					C += jobs[j][0];
+			for(int delta = 0; delta < j-k; delta++) {
+				int C = t;
+				for(int[] job : jobs) {
+					C += job[0];
 				}
-				T.put(S, Math.min( T.get(S), T.get(new int[]{S[0],S[2]+delta,S[5],S[3]}) +
-											 Math.max(0, C - jobs[S[2]][1]) +
-											 T.get(new int[]{S[5]+delta+1,S[1],S[5],C})));
+				T.put(S, Math.min( T.get(S), T.get(new int[]{i,k+delta,k,t})		+
+											 Math.max(0, C - jobs[k][1])			+
+											 T.get(new int[]{k+delta+1,j,k,C})		));
 			}
 		}
-		int Tmin = Integer.MAX_VALUE;
-		for(int i = 0; i < jobs.length; i++) {
-			Tmin = Math.min(Tmin, T.get(new int[]{0,jobs.length,i,0}));
-		}
-		return Tmin;
+
+		return T.get(new int[]{0,jobs.length,-1,0});
 	}
 }
