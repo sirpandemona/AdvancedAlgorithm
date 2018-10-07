6510cc1e73f73e8c215e88b234d9a9792c15c9cf
Vasco de Bruijn <V.deBruijn@student.tudelft.nl>
2018-10-07 17:26:57 +0200

    Cleaned project and generated jar-file

--- Exercise 1/ComputeTardiness.java
+++ Exercise 1/ComputeTardiness.java
@@ -1,12 +1,7 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class ComputeTardiness {	
 	public static ProblemInstance readInstance(String filename){
@@ -38,89 +33,11 @@ public class ComputeTardiness {
 		return instance;
 	}
 
-	// reads a problem, and outputs the result of both greedy and best-first
     public static void main (String args[]) {
-		/*ProblemInstance instance = readInstance(args[0]);
-		
-		Greedy greedy = new Greedy(instance);
-		Schedule greedySchedule = greedy.getSchedule();
-		System.out.println(greedySchedule.getTardiness());
-		
-		BestFirst bestFirst = new BestFirst(instance);
-		Schedule bestFirstSchedule = bestFirst.getSchedule();
-		System.out.println(bestFirstSchedule.getTardiness());*/
-    	
-    	runTests();
+		ProblemInstance instance = readInstance(args[0]);
+		LawlerDP lawler = new LawlerDP(instance);
+		int result =  lawler.SequenceJobs();
+    	System.out.print(result);
 	}
     
-    public static void runTests() {
-    	Path currentRelativePath = Paths.get("");
-    	String testfolder = currentRelativePath.toAbsolutePath().toString()+"\\Exercise 1\\instances"; 
-    	testfolder = testfolder.replace("\\", "/");
-    	System.out.println(testfolder);
-    	File folder = new File(testfolder);
-    	
-    	File[] testfiles = folder.listFiles();
-    	Arrays.sort(testfiles, new Comparator<File>() {
-    		public int compare(File f1, File f2) {
-    			String s1 = f1.getName();
-    			String s2 = f2.getName();
-    			s1 = s1.replace(".dat","");
-    			s2 = s2.replace(".dat","");
-    			Integer size1 = Integer.parseInt((s1.split("#")[1]));
-    			Integer size2 = Integer.parseInt((s2.split("#")[1]));
-    			return size1.compareTo(size2);
-    		}
-    	});
-    	for (int i = 0; i < testfiles.length; i++) {
-    		String fileName = testfiles[i].getName();
-    		//System.out.println(fileName);
-    		runTest(testfolder+"\\"+fileName);
-
-		}
-    }
-    
-    public static void runTest(String filename) {
-    	ProblemInstance BaseInstance = readInstance(filename);
-    	ProblemInstance[] mutatedInstances = mutateTests(BaseInstance);
-    	for(int i = 0; i<mutatedInstances.length;i++) {
-    		ProblemInstance instance = mutatedInstances[i];
-	    	int problemSize = instance.getNumJobs();
-	    	//System.out.println("Problem of size: "+problemSize);
-	    	Greedy greedy = new Greedy(instance);
-	    	long greedyStart = System.nanoTime();
-			Schedule greedySchedule = greedy.getSchedule();
-			long greedyEnd = System.nanoTime();
-			//System.out.println("Greedy result: "+greedySchedule.getTardiness()+" in time: "+((greedyEnd-greedyStart)/1000) + " ms ");
-			
-			/*BestFirst bestFirst = new BestFirst(instance);
-	    	long bfStart = System.nanoTime();
-			Schedule bestFirstSchedule = bestFirst.getSchedule();
-			long bfEnd = System.nanoTime();*/
-			//System.out.println("Best First result: "+bestFirstSchedule.getTardiness()+" in time: "+(bfEnd-bfStart) + " nanosec ");
-			
-			LawlerDP Lawler =new LawlerDP(instance);
-			long lawStart = System.nanoTime();
-			int LawlerTardiness = Lawler.SequenceJobs();
-			long lawEnd = System.nanoTime();
-	
-			//System.out.println("Lawler result: "+LawlerTardiness+ " in time: "+((lawEnd-lawStart)/1000)+ " ms ");
-			//Schedule LawlerSchedule = Lawler.getSchedule();
-			System.out.println(problemSize+" "+((greedyEnd-greedyStart)/1000)+ " "+((lawEnd-lawStart)/1000)+ ";" );
-			//System.out.println(problemSize+" "+((greedyEnd-greedyStart)/1000)+ " "+((bfEnd-bfStart)/1000)+ " "+((lawEnd-lawStart)/1000)+ ";" );
-    	}
-    }
-    
-    public static ProblemInstance[] mutateTests(ProblemInstance instance) {
-    	int size = instance.getNumJobs();
-    	int[][] jobs = instance.getJobs();
-    	ProblemInstance[] res = new ProblemInstance[size];
-    		for(int i = 0; i<size; i++) {
-    			int sSize = i+1;
-    			int[][] sJobs = Arrays.copyOfRange(jobs,0,sSize);
-    			res[i] = new ProblemInstance(sSize,sJobs);
-    		}
-    	
-    	return res;
-    }
 }
--- Exercise 1/LawlerDP.java
+++ Exercise 1/LawlerDP.java
@@ -9,19 +9,12 @@ public class LawlerDP {
 	private int[][] jobs;
 	private HashMap<String,ArrayList<Integer>> jobMapping;
 	private HashMap<String,ArrayList<int[]>> states;
-	
-	private ArrayList<int[]> L;
-	private HashMap<String, Integer> jobOrderMapping;
-	private ArrayList<Integer> ordering;
-	
+		
 	public LawlerDP(ProblemInstance instance) {
 		numJobs = instance.getNumJobs();
 		jobs = instance.getJobs();
 		states = new HashMap<String,ArrayList<int[]>>();
 		jobMapping = new HashMap<String,ArrayList<Integer>>();
-		jobOrderMapping = new HashMap<String,Integer>();
-		ordering = new ArrayList<Integer>();
-		L =new ArrayList<int[]>();
 	}
 	
 	public void sortJobsByDueTime() {
@@ -30,122 +23,36 @@ public class LawlerDP {
 			public int compare(int[] j1, int[] j2) {
 				Integer dd1 = j1[1];
 				Integer dd2 = j2[1];
-				// reverse sort on quantity
 				return dd1.compareTo(dd2);
 			}
 		});
 	}
 	
-	public Schedule getSchedule() {
-		Schedule bestSchedule = null;			
-		//magic happens here
-		sortJobsByDueTime();
-		ArrayList<int[]> states = generateStates(0,numJobs-1,-1,0);
-		//System.out.print(states);
-		return bestSchedule;
-	}
 	
-	public ArrayList<int[]> generateStatesIt(){
-		int[] startState = {0,jobs.length-1,-1,0};
-		ArrayList<int[]> queue = new ArrayList<int[]>();
-		ArrayList<Integer> order = new ArrayList<Integer>();
-		HashMap<String,Integer> jobOrderMapping = new HashMap<String,Integer>();
-		
-		queue.add(startState);
-		int idx = 0;
-		while(idx != queue.size()) {
-			int[] S = queue.get(idx);
-			int i = S[0];
-			int j = S[1];
-			int k = S[2];
-			int t = S[3];
-			String key  = i + " - " + j + " - "+ k + " - "+t;
-			if(jobOrderMapping.containsKey(key)){
-				int dept = states.get(key).size();
-				int orderidx = jobOrderMapping.get(key);
-				for (int a = orderidx; a<dept;a++) {
-					order.set(orderidx+a, idx+a);
-				}
-			}
-			else 
-			{
-				queue.addAll(generateStates(i,j,k,t));
-				jobOrderMapping.put(key,idx);
-				System.out.println("idx: "+idx +" sizeQueue"+queue.size());
-			}
-			idx++;
-		}
-		return queue;
-	}
-	
-	public boolean noTardyJob(ArrayList<Integer> subset, int t)
-	{
-		int C = t;
-		for(int job : subset) {
-			C += jobs[job][0];
-			if(jobs[job][1] > C)
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public int allTardyJobs(ArrayList<Integer> subset, int t)
-	{
-		int C = t;
-		int T = 0;
-		for(int job : subset) {
-			C += jobs[job][0];
-			if(jobs[job][1] < C)
-			{
-				return 0;
-			}
-			T += jobs[job][1] - C;
-		}
-		return T;
-	}
-	
-	public int shortcut(int i, int j, int k, int t)
-	{
-		ArrayList<Integer> subset = retrieveSubset(i,j,k);
-		if(subset.size() > 0 && t + jobs[subset.get(0)][0] > jobs[subset.get(0)][1]) {
-			int T = allTardyJobs(subset,t);
-			if(T > 0) {
-				return T;
-			}
-		} else {
-			if(noTardyJob(subset,t)) {
-				return 0;
-			}
-		}
-		return -1;
-	}
-	
-	public ArrayList<Integer> GenCandidateDeltas(ArrayList<Integer> S,  int t) {
-		int k = retrieveKPrime(S);
+	public ArrayList<Integer> GenCandidateDeltas(ArrayList<Integer> subset,  int t) {
+		int k = retrieveKPrime(subset);
 		ArrayList<Integer> sPrime2;
 		ArrayList<Integer> deltas = new ArrayList<Integer>();
 		int dk = jobs[k][1];
 		do {
-			ArrayList<Integer> sPrime = retrieveSPrime(S,dk);		
+			ArrayList<Integer> sPrime = retrieveSPrime(subset,dk);		
 			int dkPrime = t;
 			for(int job : sPrime) {
 				dkPrime += jobs[job][0];
 			}
 			while(dkPrime > dk) {
 				dk = dkPrime;
-				sPrime = retrieveSPrime(S,dk);
+				sPrime = retrieveSPrime(subset,dk);
 				dkPrime = t;
 				for(int job : sPrime) {
 					dkPrime += jobs[job][0];
 				}
 			}
-			int j = retrieveMaxJ(S,dk);
+			int j = retrieveMaxJ(subset,dk);
 			int delta  = j-k;
 			deltas.add(delta);
 			sPrime2 = new ArrayList<Integer>();
-			for(int job : S) {
+			for(int job : subset) {
 				if(jobs[job][1] > dk) {
 					sPrime2.add(job);
 				}
@@ -158,9 +65,9 @@ public class LawlerDP {
 		return deltas;	
 	}
 	
-	public int retrieveMaxJ(ArrayList<Integer> S, int dk) {
+	public int retrieveMaxJ(ArrayList<Integer> subset, int dk) {
 		int j = -1;
-		for(int job: S) {
+		for(int job: subset) {
 			if(jobs[job][1] <= dk) {
 				j = job;
 			}
@@ -168,9 +75,9 @@ public class LawlerDP {
 		return j;
 	}
 	
-	public ArrayList<Integer> retrieveSPrime(ArrayList<Integer>S,int dk){
+	public ArrayList<Integer> retrieveSPrime(ArrayList<Integer>subset,int dk){
 		ArrayList<Integer> sPrime = new ArrayList<Integer>();
-		for(int job : S) {
+		for(int job : subset) {
 			if(jobs[job][1] <= dk) {
 				sPrime.add(job);
 			}
@@ -180,39 +87,30 @@ public class LawlerDP {
 	
 	
 	public ArrayList<int[]> generateStates(int i, int j, int k, int t) {
-		int [] state = {i,j,k,t};
 		String key  = i + " - " + j + " - "+ k + " - "+t;
 		if(states.containsKey(key)){
-			//System.out.println("Got state: "+ key);
 			return states.get(key);
 		}
-		//System.out.println(key);
 		ArrayList<int[]> L = new ArrayList<int[]>();
-		ArrayList<Integer> S = retrieveSubset(i,j,k);
-		if(S.size() == 0) {
-			//L.add(state);
+		ArrayList<Integer> subset = retrieveSubset(i,j,k);
+		if(subset.size() == 0) {
 			return L;
 		}
-		else if(S.size() == 1){
-			//L.add(state);
+		else if(subset.size() == 1){
 			return L;
 		}
 
 		
-		int kPrime = retrieveKPrime(S);
-		//if(kPrime+delta+1 > j) { delta = j-(kPrime +1);} //prevent the
-		int C = retrieveC(S,kPrime,t);
-		ArrayList<Integer> deltas =  GenCandidateDeltas(S,t);
+		int kPrime = retrieveKPrime(subset);
+		int C = retrieveC(subset,kPrime,t);
+		ArrayList<Integer> deltas =  GenCandidateDeltas(subset,t);
 		int prevDelta = 0;
 		for(int di  = 0;di<deltas.size();di++) {
 			int d = deltas.get(di);
 			for(int pd = prevDelta+1 ;pd<d;pd++) {
 				C += jobs[kPrime+pd][0];
 			}
-			prevDelta =d;
-		//for(int d = 0; d <= j-kPrime;d++) {
-			//System.out.println("i:"+i+"- delta:"+delta+"- d:"+d+"- j:"+j);
-			
+			prevDelta =d;			
 			C += jobs[kPrime+d][0];
 			
 			int[] sLeft = {i,kPrime+d,kPrime,t};
@@ -232,43 +130,42 @@ public class LawlerDP {
 		if (jobMapping.containsKey(key)){
 			return jobMapping.get(key);
 		}
-		ArrayList<Integer> res = new ArrayList<Integer>(); 
-		if(i> j) {return res;}
+		ArrayList<Integer> subset = new ArrayList<Integer>(); 
+		if(i> j) {return subset;}
 		if(j>= numJobs) {j = numJobs-1;}
 		int valueK = Integer.MAX_VALUE;
 		if(k>-1) {
 			valueK = jobs[k][0];
 		}
-		for(int a =i; a<=j;a++) {
-			if(jobs[a][0] < valueK || (jobs[a][0] == valueK && a < k)) {
-				res.add(a);
+		for(int job =i; job<=j;job++) {
+			if(jobs[job][0] < valueK || (jobs[job][0] == valueK && job < k)) {
+				subset.add(job);
 			}
 		}
-		jobMapping.put(key,res);
-		return res;
+		jobMapping.put(key,subset);
+		return subset;
 	}
 	
-	public int retrieveKPrime(ArrayList<Integer> S) {
+	public int retrieveKPrime(ArrayList<Integer> subset) {
 		int kPrime =0;
 		int val = 0;
-		for(int a = 0; a < S.size();a++) {
-			int j = S.get(a);
-			if(jobs[j][0] >= val) {
-				kPrime = j;
-				val  = jobs[j][0] ;
+		for(int job: subset) {
+			if(jobs[job][0] >= val) {
+				kPrime = job;
+				val  = jobs[job][0] ;
 			}
 		}
 		return kPrime;
 	}
 	
-	public int retrieveC(ArrayList<Integer> S, int kPrime, int t) {
+	public int retrieveC(ArrayList<Integer> subset, int kPrime, int t) {
 		int time = t;
-		for(int j : S) {
-			if(j >= kPrime)
+		for(int job : subset) {
+			if(job >= kPrime)
 			{
 				break;
 			}
-			int p_j = jobs[j][0];
+			int p_j = jobs[job][0];
 			time+= p_j;
 		}
 		return time;
@@ -276,15 +173,11 @@ public class LawlerDP {
 	
 	public int SequenceJobs() {
 		sortJobsByDueTime();
-		//System.out.println("Generate States:");
 		int[] startState = {0,jobs.length-1,-1,0};
 		ArrayList<int[]> L = generateStates(0, jobs.length-1, -1, 0);
-		//ArrayList<int[]>L = generateStatesIt();
 		Collections.reverse(L);
 		L.add(startState);
 		HashMap<String,Integer> T = new HashMap<String,Integer>();
-		
-	//	System.out.println("Dynamic:");
 		
 		for(int[] S : L) {							
 			int i = S[0];
@@ -294,7 +187,6 @@ public class LawlerDP {
 			ArrayList<Integer> subset = retrieveSubset(i,j,oldk);
 			int k = retrieveKPrime(subset);
 			String key = i + " - " + j + " - " + oldk + " - " + t;
-			//System.out.println(key);
 			if(subset.size() < 1) {
 				T.put(key,0);
 			}
@@ -316,7 +208,6 @@ public class LawlerDP {
 						C += jobs[k+pd][0];
 					}
 					prevDelta =delta;
-				//for(int delta = 0; delta <= j-k; delta++) {
 					C += jobs[k+delta][0];
 					T.put(key, Math.min( T.get(key),	T.get(i + " - " + (k+delta) + " - " + k + " - " + t)		+
 												 		Math.max(0, C - jobs[k][1])									+
